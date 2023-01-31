f73258defc159f2e96860b3af34546e86c8633d9
MBA <48457177+MiguelBarriosAl@users.noreply.github.com>
2023-01-31 09:44:12 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -3,16 +3,6 @@ from constant import PATH_DIR, N_BATCH_FILES, DATE
 
 
 def main():
-    """
-        Class called "Pipeline", passing in three arguments: DATE, PATH_DIR, and N_BATCH_FILES.
-        The script then calls a method on the pipeline object called "etl_json()".
-        Taking into account that the number of files and the content of each file is unknown, for a better performance
-        and scaling of the project, the pipeline allows to configure through constants such as number
-        of batches of reading files.
-        This class has a method that receives the three parameters passed in the main function and
-        stores them as instance variables.
-        The "etl_json()" method is performs extraction, transformation, and loading (ETL) process on JSON files.
-    """
     pipeline = Pipeline(DATE, PATH_DIR, N_BATCH_FILES)
     pipeline.etl_json()
 
